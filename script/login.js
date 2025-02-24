772fe1e11647a38413cebef2d70d15dec1626258
Ashikqul <ashikqulislamrizvi77@gmail.com>
2025-02-25 00:49:17 +0600

    added all html,js or taiwind file

---
+++ script/login.js
@@ -0,0 +1,23 @@
+// console.log('hi')
+
+
+document.getElementById('login-btn').addEventListener('click',function(event){
+   
+    event.preventDefault();
+    const accountNumber = document.getElementById("Account-number").value;
+    
+const pin =document.getElementById('pin').value;
+const convertedPin = parseInt(pin)
+
+if(accountNumber.length === 11){
+    if(convertedPin===1234){
+        window.location.href="./main.html"
+    }
+    else{
+        alert('pin thik nei');
+    }
+}
+else{
+    alert('need valid number')
+}
+})
